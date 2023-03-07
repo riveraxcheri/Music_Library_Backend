0038441546000f246d219690c4edb3106d97ff38
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2023-03-06 23:58:07 -0600

    Created Superuser + Updated Admin, Models, Views files

--- songs/views.py
+++ songs/views.py
@@ -1,8 +1,11 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Song
+from .serializers import SongSerializer
 
 @api_view(['GET'])
 def songs_list(request):
+    songs = Song.objects.all()
+    serializer = SongSerializer(songs, many=True)
 
-
-    return Response('hello')
+    return Response(serializer.data)
