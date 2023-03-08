e518c218faf037390a0a1fe0049948c3faebc5e1
riveraxcheri <117052183+riveraxcheri@users.noreply.github.com>
2023-03-08 01:58:55 -0600

    Migrated songs files + Created Endpoints for all requests + Corrected path type error

--- songs/models.py
+++ songs/models.py
@@ -6,5 +6,5 @@ class Song(models.Model):
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
     album = models.CharField(max_length=255)
-    release_date = models.DateField('YYYY-MM-DD')
+    release_date = models.DateField()
     genre = models.CharField(max_length=255)
--- songs/views.py
+++ songs/views.py
@@ -1,11 +1,34 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Song
 from .serializers import SongSerializer
+from django.shortcuts import get_object_or_404
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def songs_list(request):
-    songs = Song.objects.all()
-    serializer = SongSerializer(songs, many=True)
+    if request.method == 'GET':
+        songs = Song.objects.all()
+        serializer = SongSerializer(songs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SongSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-    return Response(serializer.data)
+@api_view(['GET', 'PUT', 'DELETE'])
+def songs_detail(request, pk):
+    songs = get_object_or_404(Song, pk=pk)
+
+    if request.method == 'GET':
+        serializer = SongSerializer(songs);
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SongSerializer(songs, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        songs.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
